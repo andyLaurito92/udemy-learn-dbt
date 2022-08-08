3345cb881d9cfca1cee2dc063866f3ff9ab6fafa
andylaurito <andy.laurito@gmail.com>
2022-08-08 17:14:42 +0200

    defining ctes

--- models/example/snowflake_customer_purchase.sql
+++ models/example/snowflake_customer_purchase.sql
@@ -1,12 +1,20 @@
 {{ config(materialized='table') }}
 
+WITH sample_customer as (
+     SELECT * FROM {{ source('sample', 'customer') }} 
+),
+
+sample_orders as (
+     SELECT * FROM {{ source('sample', 'orders') }}
+)
+
 SELECT
 c.c_custkey,
 c.c_name,
 c.c_nationkey as nation,
 sum(o.o_totalprice) as total_order_price
-FROM {{ source('sample', 'customer') }} c
-LEFT JOIN {{ source('sample', 'orders') }} o
+FROM sample_customer c
+LEFT JOIN sample_orders o
 ON c.c_custkey = o.o_custkey
 GROUP BY
 c.c_custkey,
